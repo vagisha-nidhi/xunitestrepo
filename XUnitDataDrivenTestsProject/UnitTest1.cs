72049461bb152df5af7895ed76303ef843646498
Vagisha Nidhi <vagisha.nda@gmail.com>
2020-01-09 12:52:02 +0530

    Adding xunit

---
+++ XUnitDataDrivenTestsProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace XUnitDataDrivenTestsProject
+{
+    public class UnitTest1
+    {
+        [Trait("Priority", "0")]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void Test1(int data)
+        {
+            Assert.True(1 == 1);
+        }
+    }
+}
